657a4ee52c896886a8d7af89a26c4544a0f4fa99
yunhe_jia <yjia15@dons.usfca.edu>
2022-03-31 20:36:58 -0700

    docker fixed

--- flask_test.py
+++ flask_test.py
@@ -25,4 +25,4 @@ def following():
     return render_template('about_page.html',member_list = member_list)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0')
